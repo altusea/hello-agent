f3ad3650b72100721c3e74b8515ee113792f26db
altusea <zt.song@outlook.com>
2025-11-12 10:28:34 +0800

    build: 更新 pyproject.toml 中的构建系统配置

---
+++ bin/litellm_test.py
@@ -0,0 +1,15 @@
+from litellm import completion
+
+from ulib.utils.env_reader import read_deepseek_api_key
+
+messages = [{"content": "Hello, how are you?", "role": "user"}]
+
+
+response = completion(
+    base_url="https://api.deepseek.com/v1",
+    api_key=read_deepseek_api_key(),
+    model="deepseek-chat",
+    messages=messages,
+)
+
+print(response)
